bb68d72d0f3b81211c8b95516080d054e70df5b0
alejandro quesada tinco <tqaw19@gmail.com>
2019-10-21 18:18:49 -0500

    2nd commit

---
+++ University2/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace University2.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+
+        public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Range(0,5)]
+        public int Credits { get; set; }
+
+        public int DeparmentID { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        public virtual ICollection<Instructor> Intructors { get; set; }
+    }
+}
